3ef0b17aead81ce0922fa30fe67086d2e6094a5e
Diego Cominazzini <diego.cominazzini@gmail.com>
2023-04-28 21:01:23 +0100

    main-reset

--- force-app/main/default/lwc/dogPictureComponent/dogPictureComponent.js
+++ force-app/main/default/lwc/dogPictureComponent/dogPictureComponent.js
@@ -1,6 +1,58 @@
 import { LightningElement } from "lwc";
-import Dog_Picture from "@salesforce/resourceUrl/Dog_Picture";
+import BorderColliePicture from "@salesforce/resourceUrl/Dog_Picture";
+import DachsundPicture from "@salesforce/resourceUrl/dachspicture";
+import splitsdk from "@salesforce/resourceUrl/splitsdk";
+import { loadScript } from "lightning/platformResourceLoader";
+import Id from '@salesforce/user/Id';
+
+
 
 export default class DogPictureComponent extends LightningElement {
-  dogPictureURL = Dog_Picture;
+  treatment;
+  userId = Id;
+  splitIoClient;
+  
+  get dogPictureURL() {
+    if(this.treatment === 'Einstein') {
+      return BorderColliePicture;
+    } else if (this.treatment === 'Yext') {
+      return DachsundPicture;
+    } else {
+      return DachsundPicture;
+    }
+  }
+
+  get treatment_loaded () {
+    return !!this.treatment;
+  }
+
+  renderedCallback() {
+    loadScript(this, splitsdk).then(() => {
+      const factory = splitio({
+        core: {
+          authorizationKey: 'tv7jnj9t3da4vs6ebvug8cppde19v4vk0004',
+          key: this.userId,
+          trafficType: 'user'
+        },
+        startup: {
+          readyTimeout: 1.5,
+        }
+      });
+
+      this.splitIoClient = factory.client();
+
+      this.splitIoClient.on(this.splitIoClient.Event.SDK_READY, () => {
+        let treatment = this.splitIoClient.getTreatment("AB_SF_CHATBOT");
+        console.log('treament = ' + treatment);
+        this.treatment = treatment;
+      });
+    });
+  }
+
+  disconnectedCallback() {
+    this.splitIoClient.destroy();
+  }
+
+    
+
 }
